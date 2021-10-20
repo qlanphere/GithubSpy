aa381134315517b7d84dc312f346568180f33cab
benwato <benwato12@gmail.com>
2021-10-20 17:16:43 +0100

    update

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Repo = (props) => {
     return (
-        <div>
+        <div className='div'>
             <hr></hr>
             <h3>{props.name}</h3>
             <div className="repo_format">
-            <p>Language: {props.language}</p>
+            <p>{props.language}</p>
             <p>⭐: {props.stars}</p>
             <p>Last Updated: {props.updated}</p>
             </div>
